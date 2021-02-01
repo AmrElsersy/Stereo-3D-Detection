6e0b9de8dd07aa197874e0cf77f83704543840a0
AymanNasser <aymannasr43@gmail.com>
2021-02-01 05:48:23 +0200

    Type: feat Modified KittiCalib. to support loading video calib. files
    Signed-off-by: AymanNasser <aymannasr43@gmail.com>

--- visualization/KittiUtils.py
+++ visualization/KittiUtils.py
@@ -53,9 +53,13 @@ class KittiCalibration:
         image = Projection * R_Rectification * Camera3D_reference
 
     """
-    def __init__(self, calib_path):
+    def __init__(self, calib_path, from_video=False):
         self.calib_path = calib_path
-        self.calib_matrix = self.parse_calib_files()
+        self.calib_matrix = {}
+        if from_video:
+            self.calib_matrix = self.parse_calib_from_video(calib_path)
+        else:
+            self.calib_matrix = self.parse_calib_files(calib_path)
 
         self.P0 = self.calib_matrix["P0"]
         self.P1 = self.calib_matrix["P1"]
@@ -69,20 +73,51 @@ class KittiCalibration:
         # inverse of Tr_velo_cam
         self.Tr_cam_to_velo = self.inverse_Tr(self.Tr_velo_to_cam)
 
-    def parse_calib_files(self):
+    def parse_calib_files(self, calib_path):
         assert self.calib_path is not None
 
         mat_ = {}
-        with open(os.path.join(self.calib_path), 'r') as calib_file:
+        with open(os.path.join(calib_path), 'r') as calib_file:
             for line in calib_file:
                 line = line.split()
                 # Avoiding empty line exception
+                if len(line) == 0:
+                    continue
                 try:
                     mat_[line[0][:-1]] = np.array(line[1:], dtype=np.float32)
-                except:
-                    break
+                except ValueError:
+                    continue
 
         return mat_
+
+    def parse_calib_from_video(self, calib_path):
+        """ Read calibration for camera 2 from video calib files.
+            there are calib_cam_to_cam and calib_velo_to_cam under the calib_root_dir
+        """
+        assert calib_path is not None
+
+        mat_ = {}
+        cam2cam = self.parse_calib_files(
+            os.path.join(calib_path, "calib_cam_to_cam.txt")
+        )
+        velo2cam = self.parse_calib_files(
+            os.path.join(calib_path, "calib_velo_to_cam.txt")
+        )
+
+
+        mat_["P0"] = cam2cam["P_rect_00"]
+        mat_["P1"] = cam2cam["P_rect_01"]
+        mat_["P2"] = cam2cam["P_rect_02"]
+        mat_["P3"] = cam2cam["P_rect_03"]
+
+        mat_["R0_rect"] = cam2cam["R_rect_00"]
+
+        Tr_velo_to_cam = np.zeros((3, 4))
+        Tr_velo_to_cam[0:3, 0:3] = np.reshape(velo2cam["R"], [3, 3])
+        Tr_velo_to_cam[:, 3] = velo2cam["T"]
+        mat_["Tr_velo_to_cam"] = Tr_velo_to_cam
+        return mat_
+
 
     def rotx(self, t):
         """ 3D Rotation about the x-axis. """
@@ -255,3 +290,5 @@ def model_output_to_kitti_objects(pred_dict):
 
     return kitti_objects
 
+# VIDEO_CALIB_PATH = '/home/ayman/FOE-Linux/Graduation_Project/KITTI/2011_09_26_drive_0001/2011_09_26_calib/2011_09_26'
+# obj = KittiCalibration(calib_path='/home/ayman/FOE-Linux/Graduation_Project/KITTI/vis_samples/calib/006936.txt', from_video=False)
