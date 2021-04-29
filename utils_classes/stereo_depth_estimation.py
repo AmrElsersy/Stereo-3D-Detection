6ba332741efa800220987d63d9c7e724f9d78461
mamoanwar97 <mamoanwar97@gmail.com>
2021-04-29 15:38:05 +0200

    stereo demo

--- utils_classes/stereo_depth_estimation.py
+++ utils_classes/stereo_depth_estimation.py
@@ -12,10 +12,8 @@ from utils_classes.stereo_preprocessing import StereoPreprocessing
 
 # ========================= Stereo =========================
 class Stereo_Depth_Estimation:
-    def __init__(self, args, cfg):
+    def __init__(self, args):
         self.args = args
-        self.cfg = cfg
-
         self.model = self.load_model()
         self.preprocesiing = StereoPreprocessing()
 
@@ -34,51 +32,45 @@ class Stereo_Depth_Estimation:
         psuedo_pointcloud = self.disparity_to_pointcloud(disparity, calib_path)
 
         if return_disparity:
-            return disparity[-1] , psuedo_pointcloud
+            return disparity, psuedo_pointcloud
 
         return psuedo_pointcloud
 
     def stereo_to_disparity(self, imgL, imgR):
-        self.model.eval()
-
         imgL = imgL.float().cuda()
         imgR = imgR.float().cuda()
-
         with torch.no_grad():
-            startTime = time.time()
-            outputs, all_time = self.model(imgL, imgR)
-            all_time = ''.join(['At Stage {}: time {:.2f} ms ~ {:.2f} FPS\n'.format(
-                x, (all_time[x] - startTime) * 1000, 1 / ((all_time[x] - startTime))) for x in range(len(all_time))])
-            return outputs
+            start_time = time.time()
+            outputs = self.model(imgL, imgR)
+            output3 = torch.squeeze(outputs[3], 1)
+            image = output3.cpu()
+            img_cpu = np.asarray(image)
+            disp_map = img_cpu[0, :, :]
+            return disp_map
     
-    def disparity_to_pointcloud(self, disparity, calib_path):
+    def disparity_to_pointcloud(self, disp_map, calib_path):
         # get the last disparity (best accuracy)
-        output = disparity[-1]
-        output = torch.squeeze(output ,1)
-
-        img_cpu = np.asarray(output.cpu())
-        disp_map = img_cpu[0, :, :]
         calib = Calibration(calib_path)
-        lidar = gen_lidar(disp_map, calib)
-        sparse_points = gen_sparse_points(lidar)
+        lidar = self.gen_lidar(disp_map, calib)
+        sparse_points = self.gen_sparse_points(lidar)
         return sparse_points
 
-def gen_lidar(disp_map, calib, max_high=1):
-    disp_map = (disp_map*255).astype(np.float32)/255.
-    lidar = project_disp_to_points(calib, disp_map, max_high)
-    lidar = np.concatenate([lidar, np.ones((lidar.shape[0], 1))], 1)
-    lidar = lidar.astype(np.float32)
-    return lidar
+    def gen_lidar(disp_map, calib, max_high=1):
+        disp_map = (disp_map*255).astype(np.float32)/255.
+        lidar = project_disp_to_points(calib, disp_map, max_high)
+        lidar = np.concatenate([lidar, np.ones((lidar.shape[0], 1))], 1)
+        lidar = lidar.astype(np.float32)
+        return lidar
 
-def gen_sparse_points(lidar, H=64, W=512, D=700, slice=1):
-    pc_velo = lidar.reshape((-1, 4))
-    valid_inds =    (pc_velo[:, 0] < 120)    & \
-                    (pc_velo[:, 0] >= 0)     & \
-                    (pc_velo[:, 1] < 50)     & \
-                    (pc_velo[:, 1] >= -50)   & \
-                    (pc_velo[:, 2] < 1.5)    & \
-                    (pc_velo[:, 2] >= -2.5)
-    pc_velo = pc_velo[valid_inds]
-    sparse_points = pto_ang_map(pc_velo, H=H, W=W, slice=slice)
-    sparse_points = sparse_points.astype(np.float32)
-    return sparse_points
+    def gen_sparse_points(lidar, H=64, W=512, D=700, slice=1):
+        pc_velo = lidar.reshape((-1, 4))
+        valid_inds =    (pc_velo[:, 0] < 120)    & \
+                        (pc_velo[:, 0] >= 0)     & \
+                        (pc_velo[:, 1] < 50)     & \
+                        (pc_velo[:, 1] >= -50)   & \
+                        (pc_velo[:, 2] < 1.5)    & \
+                        (pc_velo[:, 2] >= -2.5)
+        pc_velo = pc_velo[valid_inds]
+        sparse_points = pto_ang_map(pc_velo, H=H, W=W, slice=slice)
+        sparse_points = sparse_points.astype(np.float32)
+        return sparse_points
